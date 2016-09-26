bd4fe242444755c980d365789de2b3d582bf73fc
jessie-chuang <chy_jessie@hotmail.com>
2016-09-26 12:59:23 +0800

    hw1 105753008

--- hw1/hw1_105753008.R
+++ hw1/hw1_105753008.R
@@ -1,16 +1,16 @@
-print("hello world123")
-
-pam1<-read.table("pam1.txt")
+#讀取pam1
+pam1<-read.table("pam1.txt")   
 dim(pam1)
 str(pam1)
+#矩陣
 b<-as.matrix(pam1)
 c<-as.matrix(pam1)
+#數字太大，先除10000
 b<-b/10000
 c<-c/10000
-
+#pam1的250次方
 for (x in c(1:249)){c <-b %*% c}  
 
 c <-c*100
-print(c)
 
 write.table(c, file = "pam250.txt")
